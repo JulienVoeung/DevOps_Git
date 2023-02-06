7cf28780147d7d9a2622c1ec7253d39016005873
JulienVoeung <voeung.julien@gmail.com>
2023-02-06 11:47:23 +0100

    task 5

--- main_file.ps1
+++ main_file.ps1
@@ -8,4 +8,8 @@ Write-Host “Task3”
 =======
 Write-Host “Task3”
 >>>>>>> 23775065a35306a4174e643b6b79b43fbd3d9fd1
+<<<<<<< HEAD
 Write-Host “Task4”
+=======
+Write-Host “Task5”
+>>>>>>> e951aa3 (task 5)
